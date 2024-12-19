6fda60ed99fcc4f0fae750ebdc1876ba7a5f51ad
Aaron Kapor <aaronkapor@gmail.com>
2024-12-19 06:24:24 -0800

    adding backend

--- app/be.py
+++ app/be.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def connect():
-    conn=sqlite3.connect("bookdesk")
+    conn=sqlite3.connect("bookdesk.db")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS library1 (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, type TEXT, genre TEXT, isbn INTEGER)")
     conn.commit()
