61b981d356e195f78403271bdf9a2e0da25d854b
Aaron Kapor <aaronkapor@gmail.com>
2024-12-20 09:20:13 -0800

    updating backend functions

--- app/backend.py
+++ app/backend.py
@@ -3,14 +3,14 @@ import sqlite3
 def connect():
     conn=sqlite3.connect("bookdesk.db")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS library1 (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    cur.execute("CREATE TABLE IF NOT EXISTS library1 (id INTEGER PRIMARY KEY, title text, author text, year integer, publisher text, type text, artist text)")
     conn.commit()
     conn.close()
 
-def insert(title,author,year,isbn):
+def insert(title,author,year,publisher,type,artist):
     conn=sqlite3.connect("bookdesk.db")
     cur=conn.cursor()
-    cur.execute("INSERT INTO library1 VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+    cur.execute("INSERT INTO library1 VALUES (NULL,?,?,?,?,?,?)",(title,author,year,publisher,type,artist))
     conn.commit()
     conn.close()
     view()
@@ -23,30 +23,30 @@ def view():
     conn.close()
     return rows
 
-# def search(title="",author="",year="",isbn=""):
-#     conn=sqlite3.connect("bookdesk.db")
-#     cur=conn.cursor()
-#     cur.execute("SELECT * FROM library1 WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
-#     rows=cur.fetchall()
-#     conn.close()
-#     return rows
+def search(title="",author="",year="",publisher="",type="",artist=""):
+    conn=sqlite3.connect("bookdesk.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM library1 WHERE title=? OR author=? OR year=? OR publisher=? OR type=? OR artist=?", (title,author,year,publisher,type,artist))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
 
-# def delete(id):
-#     conn=sqlite3.connect("bookdesk.db")
-#     cur=conn.cursor()
-#     cur.execute("DELETE FROM library1 WHERE id=?",(id,))
-#     conn.commit()
-#     conn.close()
+def delete(id):
+    conn=sqlite3.connect("bookdesk.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM library1 WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
 
-# def update(id,title,author,year,isbn):
-#     conn=sqlite3.connect("bookdesk.db")
-#     cur=conn.cursor()
-#     cur.execute("UPDATE library1 SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
-#     conn.commit()
-#     conn.close()
+def update(id,title,author,year,publisher,type,artist):
+    conn=sqlite3.connect("bookdesk.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE library1 SET title=?, author=?, year=?, publisher=?, type=?, artist=? WHERE id=?",(title,author,year,publisher,type,artist,id))
+    conn.commit()
+    conn.close()
 
 connect()
-#insert("The Sun","John Smith",1918,913123132)
+insert("The Sun","John Smith",1918,913123132, 913123132, 913123132)
 #delete(3)
 #update(4,"The moon","John Smooth",1917,99999)
 #print(view())
--- app/frontend.py
+++ app/frontend.py
@@ -1,18 +1,7 @@
 from tkinter import *
 import backend
 
-# def get_selected_row(event):
-#     global selected_tuple
-#     index=list1.curselection()[0]
-#     selected_tuple=list1.get(index)
-#     e1.delete(0,END)
-#     e1.insert(END,selected_tuple[1])
-#     e2.delete(0,END)
-#     e2.insert(END,selected_tuple[2])
-#     e3.delete(0,END)
-#     e3.insert(END,selected_tuple[3])
-#     e4.delete(0,END)
-#     e4.insert(END,selected_tuple[4])
+
 
 def view_command():
     list1.delete(0,END)
@@ -21,19 +10,19 @@ def view_command():
 
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),publisher_text.get(),type_text.get(),artist_text.get()):
         list1.insert(END,row)
 
 def add_command():
-    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    backend.insert(title_text.get(),author_text.get(),year_text.get(),publisher_text.get(),type_text.get(),artist_text.get())
     list1.delete(0,END)
-    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),publisher_text.get(),type_text.get(),artist_text.get()))
 
 def delete_command():
     backend.delete(selected_tuple[0])
 
 def update_command():
-    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),publisher_text.get(),type_text.get(),artist_text.get())
 
 window=Tk()
 
@@ -48,8 +37,14 @@ l2.grid(row=0,column=2)
 l3=Label(window,text="Year")
 l3.grid(row=1,column=0)
 
-l4=Label(window,text="ISBN")
+l4=Label(window,text="publisher")
 l4.grid(row=1,column=2)
+
+l5=Label(window,text="type")
+l5.grid(row=2,column=0)
+
+l6=Label(window,text="artist")
+l6.grid(row=2,column=2)
 
 title_text=StringVar()
 e1=Entry(window,textvariable=title_text)
@@ -63,9 +58,17 @@ year_text=StringVar()
 e3=Entry(window,textvariable=year_text)
 e3.grid(row=1,column=1)
 
-isbn_text=StringVar()
-e4=Entry(window,textvariable=isbn_text)
+publisher_text=StringVar()
+e4=Entry(window,textvariable=publisher_text)
 e4.grid(row=1,column=3)
+
+type_text=StringVar()
+e5=Entry(window,textvariable=type_text)
+e5.grid(row=2,column=1)
+
+artist_text=StringVar()
+e6=Entry(window,textvariable=artist_text)
+e6.grid(row=2,column=3)
 
 list1=Listbox(window, height=6,width=35)
 list1.grid(row=2,column=0,rowspan=6,columnspan=2)
@@ -79,21 +82,21 @@ sb1.configure(command=list1.yview)
 # list1.bind('<<ListboxSelect>>',get_selected_row)
 
 b1=Button(window,text="View all", width=12,command=view_command)
-b1.grid(row=2,column=3)
+b1.grid(row=3,column=3)
 
 b2=Button(window,text="Search entry", width=12,command=search_command)
-b2.grid(row=3,column=3)
+b2.grid(row=4,column=3)
 
 b3=Button(window,text="Add entry", width=12,command=add_command)
-b3.grid(row=4,column=3)
+b3.grid(row=5,column=3)
 
 b4=Button(window,text="Update selected", width=12,command=update_command)
-b4.grid(row=5,column=3)
+b4.grid(row=6,column=3)
 
 b5=Button(window,text="Delete selected", width=12,command=delete_command)
-b5.grid(row=6,column=3)
+b5.grid(row=7,column=3)
 
 b6=Button(window,text="Close", width=12,command=window.destroy)
-b6.grid(row=7,column=3)
+b6.grid(row=8,column=3)
 
 window.mainloop()
